938cc73f01bcf08bca3a5fdac6224ac2ae1e2781
PlunkettSean <seanplunkett474@gmail.com>
2021-11-15 09:34:01 -0500

    Reanimated 2 Error supression

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Router from './src/navigation/Router';
-import { StatusBar } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 StatusBar.setBarStyle('dark-content', true);
 const database = require('./src/components/Handlers/database.js')
 
@@ -24,5 +24,5 @@ const App = () => {
 
   return <Router />;
 };
-
+LogBox.ignoreLogs(['Reanimated 2']);
 export default App;
